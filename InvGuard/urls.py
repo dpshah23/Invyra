ffb9e570811575a8cc8496f892e75e21e249e745
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2026-03-05 16:51:33 +0530

    Added Apps

--- InvGuard/urls.py
+++ InvGuard/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/',include('auth1.urls'))
+    path('auth/',include('auth1.urls')),
+    
 
 ]
