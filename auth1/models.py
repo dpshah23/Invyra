09a9849b4da0cbe089a5971b1c6e346b3206bce9
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2026-03-05 17:52:17 +0530

    Auth update

--- auth1/models.py
+++ auth1/models.py
@@ -10,13 +10,29 @@ class UserCustom(models.Model):
     email=models.EmailField(unique=True)    
     name=models.CharField(max_length=150)
     password=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    lst_login=models.DateTimeField(auto_now=True)
+    company_name=models.CharField(max_length=255, blank=True, null=True)
+    status=models.CharField(max_length=20, default='active')
+
+    def generateusername(self,email):
+       
+        base_username = email.split('@')[0]
+        unique_username = base_username
+        counter = 1
+
+        while UserCustom.objects.filter(username=unique_username).exists():
+            unique_username = f"{base_username}{counter}"
+            counter += 1
+
+        return unique_username
+
 
     def hashpassword(self,password):
-        # Hash the password using bcrypt
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
         return hashed_password.decode('utf-8')
     
     def check_password(self, password):
-        
+    
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
--- auth1/views.py
+++ auth1/views.py
@@ -10,13 +10,14 @@ def signup(request):
 
     if request.method == 'POST':
         print("Signup form submitted")
-        username = request.POST.get('username')
+        
         email = request.POST.get('email')
         name = request.POST.get('name')
         password = request.POST.get('password')
+        compname=request.POST.get('compname')
 
-     
-        user = User(username=username, email=email, name=name)
+        username=User().generateusername(email)
+        user = User(username=username,email=email,name=name,lstlogin=None,company_name=compname)
         user.password = user.hashpassword(password)  
         user.save() 
     return render(request,'login.html')
@@ -32,10 +33,12 @@ def login(request):
                 if user.check_password(password):
                     print("Login successful")
                     session = request.session
-                    session['user_id'] = user.id
+                    # session['user_id'] = user.id
                     session['username'] = user.username
                     session['name'] = user.name
 
+                    # update last login time to now
+                    
                     return render(request,'welcome.html',{'name':user.name})
                 else:
                     print("Unexpected error occured")
