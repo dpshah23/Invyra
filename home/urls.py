6721d3f97646daf0aeba8a3625191fcb3075b050
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2026-03-06 16:04:41 +0530

    Updated Login pages ui and all page navbar

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view,about,contact 
 
 urlpatterns = [
-    path('',home_view,name="home")
+    path('',home_view,name="home"),
+    path('about_us/',about,name="about_us"),
+    path('contact_us/',contact,name="contact_us")
 ]
